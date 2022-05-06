81f033298e42809572f30be887f763d21616db0d
AlexGol005 <pesok1985>
2022-05-06 16:17:40 +0300

    06.05.2022 PA

--- ChemJournal/settings.py
+++ ChemJournal/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n!wuq6vjp-1u#_g)_18)vn2k8nd@!$8n4&h%gytgya_ig*0lm8'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'true'
+ALLOWED_HOSTS = [host.strip() for host in
+                 os.getenv('ALLOWED_HOSTS').split(',')]
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
