bc0d4ba29d4f8498c58026e324a9624a058f9777
AlexGol005 <pesok1985>
2022-05-09 20:08:42 +0300

    1

--- ChemJournal/settings.py
+++ ChemJournal/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
